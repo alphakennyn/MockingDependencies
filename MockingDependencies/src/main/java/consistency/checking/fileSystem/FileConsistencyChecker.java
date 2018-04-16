ef696fec5896765e9b6d95f4d6e7853180480274
Ehsan Mirsaeedi <mirsaeedi@outlook.com>
2018-04-16 15:01:05 -0400

    implement consistenct checker

--- MockingDependencies/src/main/java/consistency/checking/fileSystem/FileConsistencyChecker.java
+++ MockingDependencies/src/main/java/consistency/checking/fileSystem/FileConsistencyChecker.java
@@ -3,6 +3,7 @@ package consistency.checking.fileSystem;
 import consistency.checking.InMemoryFileSystemChecksumDataStore;
 import consistency.checking.contracts.IConsistencyChecker;
 import consistency.checking.contracts.INotifier;
+import consistency.checking.fileSystem.FileSystemUnit.FileSystemUnitType;
 import consistency.checking.main.FileSystemHashGenerator;
 
 public class FileConsistencyChecker implements IConsistencyChecker {
@@ -13,6 +14,9 @@ public class FileConsistencyChecker implements IConsistencyChecker {
 	private FileSystemDataStore fileStore;
 	private FileSystemHashGenerator hashGenerator;
 
+	
+	// Dependency Inversion & Injection
+	// FileSystemChecker get's all the required dependencies through it's constructor
 	public FileConsistencyChecker(INotifier notifer,
 			InMemoryFileSystemChecksumDataStore checksumStore, // for each file/directory holds the corresponding hash value
 			FileSystemDataStore fileStore, // contains all the files and directories
@@ -33,11 +37,29 @@ public class FileConsistencyChecker implements IConsistencyChecker {
 	@Override
 	public void checkConsistency() {
 
-		// TODO
-		// 1. iterate over items in fileStore and compute the checksum of files/directories
-		// check whether the computed checksum is equal to the previously computed checksum
-		// 2. in case of checksum violation, use notifier to print the error
 		
+		for(FileSystemUnit unit:fileStore) {
+			
+			// a FileSystemUnit can be a File or a Directory. It's an abstraction over different kind of File System entities 
+			// hashGeneration gets a FileSystemUnit but internally it handles Files and Directories differently
+			
+			String newChecksum = this.hashGenerator.hash(unit); 
+			
+			// getting the previously computed checksum of the entity
+			String oldChecksum=checksumStore.getDataUnit(unit.getPath()).getValue();
+			
+			if(!newChecksum.equals(oldChecksum)) {
+				
+				if(unit.getFileSystemUnitType()==FileSystemUnitType.Directory){
+					// we use INotifier interface to notify the admin. The concrete implementations has been injected through constructor.
+					this.notifier.notifyOnViolationDetectedOnFile("Violation on Directory, path: " +unit.getPath());
+				}else if(unit.getFileSystemUnitType()==FileSystemUnitType.File){
+					this.notifier.notifyOnViolationDetectedOnFile("Violation on File, path: " +unit.getPath());
+				}
+			}
+				
+			
+		}
 		
 	}
 	
